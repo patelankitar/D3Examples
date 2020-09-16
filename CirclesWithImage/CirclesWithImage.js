67bfb2ab6a0d01e30e9efb02743b69c9d0c5314f
patelankitar <62406994+patelankitar@users.noreply.github.com>
2020-09-16 15:22:18 -0500

    Update CirclesWithImage.js

--- CirclesWithImage/CirclesWithImage.js
+++ CirclesWithImage/CirclesWithImage.js
@@ -4,19 +4,24 @@ var width = 500,
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-    .style("background-color", "red")
+    .style("background-color", "black")
 	
 
 d3.csv("data.csv", function(data){
-   var circles = svg.selectAll("circle")
-                           .data(data)
-                           .enter()
-                           .append("circle");
-	
-	//Add the circle attributes
+ var jsonCircles = [
+   { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+   { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+   { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+ 
+
+var circles = svg.selectAll("circle")
+                          .data(jsonCircles)
+                          .enter()
+                          .append("circle");
+
 var circleAttributes = circles
-                       .attr("cx", 100)
-                       .attr("cy", 200)
-                       .attr("r", 50)
-                       .style("fill", "blue");
+                       .attr("cx", function (d) { return d.x_axis; })
+                       .attr("cy", function (d) { return d.y_axis; })
+                       .attr("r", function (d) { return d.radius; })
+                       .style("fill", function(d) { return d.color; });
 });
