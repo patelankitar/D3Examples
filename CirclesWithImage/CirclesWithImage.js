2131b2bafd298236265b65bc59d9141615d5470b
patelankitar <62406994+patelankitar@users.noreply.github.com>
2020-09-16 16:31:50 -0500

    Update CirclesWithImage.js

--- CirclesWithImage/CirclesWithImage.js
+++ CirclesWithImage/CirclesWithImage.js
@@ -4,18 +4,19 @@ var width = 500,
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-    .style("background-color", "blue")
+    .style("background-color", "yellow")
 	
 
-d3.csv("data.csv", function(data){
+d3.csv("data.csv", function(csvdata){
  var jsonCircles = [
    { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
    { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
    { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
  
+	console.log(csvdata);
 
 var circles = svg.selectAll("circle")
-                          .data(data)
+                          .data(csvdata)
                           .enter()
                           .append("circle");
 
