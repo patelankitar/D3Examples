949e3c0af799d0ea1f91d5ec49c841e37ce034a8
patelankitar <62406994+patelankitar@users.noreply.github.com>
2020-09-17 10:09:37 -0500

    Update CirclesWithImage.js

--- CirclesWithImage/CirclesWithImage.js
+++ CirclesWithImage/CirclesWithImage.js
@@ -1,9 +1,9 @@
-var width = 500 , height = 500;
+var width = 800 , height = 500;
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-    .style("background-color", "yellow")
+    .style("background-color", "lightblue")
 
 d3.csv("data.csv", function(CSVdata){
 
@@ -15,6 +15,6 @@ d3.csv("data.csv", function(CSVdata){
 	var circleAttributes = circles
                        .attr("cx", function (d) { return d.cx; })
                        .attr("cy", function (d) { return d.cy; })
-                       .attr("r", function (d) { return 100; })
-                       .style("fill", "red");
+                       .attr("r", function (d) { return d.weightage; })
+                       .style("fill", "yellow");
 });
